a0d332e2e240c1e49e2ac11d0358fd0151f8dcf6
isaac192063 <ichinchillac@ufpso.edu.co>
2022-11-26 21:03:48 -0500

    basico

---
+++ varios componentes/varios-componentes/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(nombre, i) in nombres" :key="i">{{ nombre }}</li>
+    </ul>
+    <input type="text" placeholder="nombres" v-model="nombre" />
+    <button @click="agregarNombre">agregar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: function () {
+    return {
+      nombres: [],
+      nombre: "",
+    };
+  },
+  methods: {
+    agregarNombre() {
+      const nombre = this.nombre.trim();
+      if (nombre == "") {
+        return;
+      }
+      this.nombres.push(nombre);
+      this.nombre = "";
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
